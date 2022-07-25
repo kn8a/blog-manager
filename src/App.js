65e39657526aaade3f2a6fbd7c19d98087903399
kn8a <88045655+kn8a@users.noreply.github.com>
2022-07-25 22:02:00 +0700

    login working, token received

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from 'axios'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Main from './pages/Main'
+import PostEdit from "./pages/PostEdit";
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Login from "./pages/Login";
+
+
+
+
 
 function App() {
+
+  const [token, setToken] = useState(null)
+  
+  const tokenToState = (JWTtoken) => {
+    setToken(JWTtoken)
+  }
+
   return (
-    <div className="App">
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login token={token} tokenToState={tokenToState}/>} />
+        <Route path='/' element={<Main token={token}/>} />
+        <Route path='/posts/:postId' exact element={<PostEdit token={token}/>} />
+      </Routes>
+    </Router>
+    <ToastContainer/>
+    </>
   );
 }
 
